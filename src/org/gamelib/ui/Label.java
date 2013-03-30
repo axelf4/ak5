da615ea8682b925b613a6209e0148572cafa1cfc
pwnedary <4x31@mail.com>
2013-03-30 18:17:12 +0100

    added labels

---
+++ src/org/gamelib/ui/Label.java
@@ -0,0 +1,47 @@
+/**
+ * 
+ */
+package org.gamelib.ui;
+
+import java.awt.Graphics2D;
+import java.util.List;
+
+import org.gamelib.View;
+import org.gamelib.Handler.Event;
+
+/**
+ * @author pwnedary
+ *
+ */
+public class Label extends Component {
+	
+	private String s;
+
+	/**
+	 * @param view
+	 */
+	public Label(View view, String s) {
+		super(view);
+		this.s = s;
+	}
+
+	/* (non-Javadoc)
+	 * @see org.gamelib.Handler#handle(org.gamelib.Handler.Event)
+	 */
+	@Override
+	public void handle(Event event) {
+		if (event instanceof Event.Draw) {
+			Graphics2D g2d = ((Event.Draw) event).graphics2d;
+			g2d.drawString(s, rectangle.x, rectangle.y);
+		}
+	}
+
+	/* (non-Javadoc)
+	 * @see org.gamelib.Handler#handlers(java.util.List)
+	 */
+	@Override
+	public void handlers(List<Class<? extends Event>> list) {
+		list.add(Event.Draw.class);
+	}
+
+}
