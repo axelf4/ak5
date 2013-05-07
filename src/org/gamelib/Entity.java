5a93e9eb0b0e05421615a4432ed8b50e9c31d034
pwnedary <4x31@mail.com>
2013-05-07 08:22:08 +0200

    updated entity class and todos

--- src/org/gamelib/Entity.java
+++ src/org/gamelib/Entity.java
@@ -3,88 +3,45 @@
  */
 package org.gamelib;
 
-import org.gamelib.backend.Graphics;
-
-
 /**
- * An entity with a coordinate in a 2d space.
- * 
- * @author Axel
+ * An object with coordinates in a 2d space.
+ * @author pwnedary
  */
-public interface Entity {
-	
-	public void update(float delta);
-	public void draw(Graphics g, float delta);
-	
-	/**
-	 * @author Axel
-	 * @return the x-axis coordinate.
-	 */
+public interface Entity extends Updateable, Drawable {
+
+	/** @return the x-axis coordinate. */
 	public float getX();
 
-	/**
-	 * @author Axel
-	 * @param the new x-axis coordinate.
-	 */
+	/** @param the new x-axis coordinate. */
 	public void setX(float x);
 
-	/**
-	 * @author Axel
-	 * @return the y-axis coordinate.
-	 */
+	/** @return the y-axis coordinate. */
 	public float getY();
 
-	/**
-	 * @author Axel
-	 * @param the new y-axis coordinate.
-	 */
+	/** @param the new y-axis coordinate. */
 	public void setY(float y);
 
-	/**
-	 * @author Axel
-	 * @return the last x-axis coordinate.
-	 */
+	/** @return the last x-axis coordinate. */
 	public float getLastX();
 
-	/**
-	 * @author Axel
-	 * @param the last new x-axis coordinate.
-	 */
+	/** @param the last new x-axis coordinate. */
 	public void setLastX(float lastX);
 
-	/**
-	 * @author Axel
-	 * @return the last x-axis coordinate.
-	 */
+	/** @return the last x-axis coordinate. */
 	public float getLastY();
 
-	/**
-	 * @author Axel
-	 * @param the last new x-axis coordinate.
-	 */
+	/** @param the last new x-axis coordinate. */
 	public void setLastY(float lastY);
 
-	/**
-	 * @author Axel
-	 * @return the speed traveling the x-axis coordinate.
-	 */
+	/** @return the speed traveling the x-axis coordinate. */
 	public float getDX();
 
-	/**
-	 * @author Axel
-	 * @param the new speed traveling the x-axis coordinate.
-	 */
+	/** @param the new speed traveling the x-axis coordinate */
 	public void setDX(float hspeed);
 
-	/**
-	 * @author Axel
-	 * @return the speed traveling the y-axis coordinate.
-	 */
+	/** @return the speed traveling the y-axis coordinate. */
 	public float getDY();
 
-	/**
-	 * @author Axel
-	 * @param the new speed traveling the y-axis coordinate.
-	 */
+	/** @param the new speed traveling the y-axis coordinate. */
 	public void setDY(float vspeed);
 }
