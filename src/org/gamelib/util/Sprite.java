2743685da9041534ade4d90c94c704f11b5fc084
pwnedary <4x31@mail.com>
2013-07-13 21:04:39 +0200

    spritesheets now use ints instead of strings as ids

--- src/org/gamelib/util/Sprite.java
+++ src/org/gamelib/util/Sprite.java
@@ -8,104 +8,109 @@ import java.awt.Rectangle;
 import org.gamelib.backend.Graphics;
 import org.gamelib.backend.Image;
 
-
 /**
- * An two-dimensional animation integrated into an larger image.
- * 
+ * A two-dimensional animation integrated into an larger image.
  * @author pwnedary
- * 
  */
 public class Sprite {
 
-	public String name;
 	public Rectangle[] subImages;
-	/** The frame index. */
-	public int frame;
+	/** the current frame */
+	int frame;
+	/** the duration of each frame */
 	public int duration;
-	private float step_counter;
+	float step_counter;
+	boolean flipped = false;
 	public double rotation;
-
-	public Sprite(String name, int duration, Rectangle... subImages) {
-		this.name = name;
+	@Deprecated
+	public String name;
+	
+	public Sprite(int duration, Rectangle... subImages) {
 		this.subImages = subImages;
 		this.duration = duration;
 	}
 
-	public Sprite(String name, Rectangle... subImages) {
-		this(name, 5, subImages);
+	public Sprite(Rectangle... subImages) {
+		this(5, subImages);
 	}
 
-	public Sprite(Rectangle... subImages) {
-		this("", subImages);
-	}
-	
-	public void draw(Graphics g, Image image, int x, int y, int flip) {
+	/** Draws the current sprite. */
+	public void draw(Graphics g, float delta, Image image, int x, int y) {
 		Rectangle rectangle = subImages[frame];
 		int sx = rectangle.x;
 		int sy = rectangle.y;
 		int width = rectangle.width;
 		int height = rectangle.height;
-		
+		int flip = (flipped ? width : 0);
+
 		// g.rotate(rotation, x + width / 2, y + height / 2);
 		g.drawImage(image, x + flip, y, x + width - flip, y + height, sx, sy, sx + width, sy + height);
 		// g.rotate(-rotation, x + width / 2, y + height / 2);
-		
-		if (++step_counter > duration) {
-			step_counter = 0;
-			if (++frame >= subImages.length)
-				frame = 0;
-		}
-	}
-	
-	public void draw(Graphics g, float delta, Image image, int x, int y, int flip) {
-		Rectangle rectangle = subImages[frame];
-		int sx = rectangle.x;
-		int sy = rectangle.y;
-		int width = rectangle.width;
-		int height = rectangle.height;
-		
-		// g.rotate(rotation, x + width / 2, y + height / 2);
-		g.drawImage(image, x + flip, y, x + width - flip, y + height, sx, sy, sx + width, sy + height);
-		// g.rotate(-rotation, x + width / 2, y + height / 2);
-		
-		step_counter += 35.0 * delta;
-		// System.out.println("counter: " + step_counter + " delta: " + delta);
-		if (step_counter > duration) {
-			step_counter = 0;
-			if (++frame >= subImages.length)
-				frame = 0;
-			// System.out.println("frame: " + frame);
-		}
+
+		if ((step_counter += 35f * delta) > duration && (step_counter = 0) == 0 && ++frame >= subImages.length) frame = 0;
 	}
 
 	/**
 	 * loads an image animation
-	 * 
 	 * @param nFrames amount of frames
 	 * @param x start x coordinate
 	 * @param y start y coordinate
 	 * @param width width of each subimage
 	 * @param height height of each subimage
-	 * @param hAmount amount on each row
+	 * @param row amount on each row
 	 * @param spacing pixels between the subimages
 	 * @return rectangles as subimages
 	 */
-	public static final Rectangle[] load(int nFrames, int x, int y, int width, int height, int hAmount, int spacing) {
-		// TODO
+	public static final Rectangle[] load(int nFrames, int x, int y, int width, int height, int row, int spacing) {
 		Rectangle[] subImages = new Rectangle[nFrames];
-		for (int i = 0; i < nFrames; i++) {
-			// System.out.println("x: " + 1 * (i % hAmount) + " y: " + 1 * (i / hAmount));
-			subImages[i] = new Rectangle(x + (width + spacing) * (i % hAmount), y + (height + spacing) * (i / hAmount), width, height);
-		}
+		for (int i = 0; i < nFrames; i++)
+			subImages[i] = new Rectangle(x + (width + spacing) * (i % row), y + (height + spacing) * (i / row), width, height);
 		return subImages;
 	}
 
+	/** @return the current frame */
 	public int getFrame() {
 		return frame;
 	}
 
 	public void setFrame(int frame) {
 		this.frame = frame;
+	}
+	
+	public void setHorizontalFlip(boolean flipped) {
+		this.flipped = flipped;
+	}
+	
+	/* OLD DEPRECATED METHODS */
+	
+	@Deprecated
+	public Sprite(String name, int duration, Rectangle... subImages) {
+		this.name = name;
+		this.subImages = subImages;
+		this.duration = duration;
+	}
+
+	@Deprecated
+	public Sprite(String name, Rectangle... subImages) {
+		this(name, 5, subImages);
+	}
+	
+	@Deprecated
+	public void draw(Graphics g, Image image, int x, int y, int flip) {
+		Rectangle rectangle = subImages[frame];
+		int sx = rectangle.x;
+		int sy = rectangle.y;
+		int width = rectangle.width;
+		int height = rectangle.height;
+
+		// g.rotate(rotation, x + width / 2, y + height / 2);
+		g.drawImage(image, x + flip, y, x + width - flip, y + height, sx, sy, sx + width, sy + height);
+		// g.rotate(-rotation, x + width / 2, y + height / 2);
+
+		if (++step_counter > duration) {
+			step_counter = 0;
+			if (++frame >= subImages.length) frame = 0;
+		}
 	}
 
 }
--- src/org/gamelib/util/SpriteSheet.java
+++ src/org/gamelib/util/SpriteSheet.java
@@ -4,25 +4,20 @@
 package org.gamelib.util;
 
 import java.awt.Rectangle;
-import java.awt.image.ImageObserver;
 
 import org.gamelib.backend.Graphics;
 import org.gamelib.backend.Image;
 
 /**
  * An image divided into animated sprites.
- * 
  * @author pwnedary
- * 
  */
 public class SpriteSheet {
 
 	private Image image;
-	/**
-	 * All sprites
-	 */
+	/** the array containing all sprites */
 	private Sprite[] spriteSheet;
-
+	/** the current sprite index */
 	private int sprite;
 	private boolean flippedHorizontal;
 
@@ -33,7 +28,46 @@ public class SpriteSheet {
 		this.image = image;
 		this.spriteSheet = spriteSheet;
 	}
+
+	public void draw(Graphics g, float delta, int x, int y) {
+		Sprite sprite2 = spriteSheet[sprite];
+		sprite2.setHorizontalFlip(flippedHorizontal);
+
+		spriteSheet[sprite].draw(g, delta, image, x, y);
+	}
+
+	/**
+	 * @param sprite the sprite to add
+	 * @return the identifier of the added sprite
+	 */
+	public int addSprite(Sprite sprite) {
+		Sprite[] tmp = spriteSheet;
+		spriteSheet = new Sprite[tmp.length + 1];
+		int i;
+		for (i = 0; i < tmp.length; i++)
+			spriteSheet[i] = tmp[i];
+		spriteSheet[i] = sprite;
+		return i;
+	}
+
+	public void setSprite(int id) {
+		if (id < 0 || id >= spriteSheet.length) throw new ArrayIndexOutOfBoundsException("No sprite with id: " + id);
+		this.sprite = id;
+	}
+
+	/** @return the sprite with the <code>id</code> */
+	public Sprite getSprite(int id) {
+		return spriteSheet[id];
+	}
+
+	/** @return the current sprite */
+	public Sprite getSprite() {
+		return getSprite(sprite);
+	}
 	
+	/* OLD METHODS */
+	
+	@Deprecated
 	public void draw(Graphics g, int x, int y) {
 		Sprite sprite2 = spriteSheet[sprite];
 		Rectangle rectangle = spriteSheet[sprite].subImages[sprite2.frame];
@@ -41,24 +75,22 @@ public class SpriteSheet {
 		int sy = rectangle.y;
 		int width = rectangle.width;
 		int height = rectangle.height;
-		
+
 		int flip = (flippedHorizontal ? width : 0);
 		spriteSheet[sprite].draw(g, image, x, y, flip);
 	}
-	
-	public void draw(Graphics g, float delta, int x, int y) {
-		Sprite sprite2 = spriteSheet[sprite];
-		Rectangle rectangle = spriteSheet[sprite].subImages[sprite2.frame];
-		int sx = rectangle.x;
-		int sy = rectangle.y;
-		int width = rectangle.width;
-		int height = rectangle.height;
-		
-		int flip = (flippedHorizontal ? width : 0);
-		spriteSheet[sprite].draw(g, delta, image, x, y, flip);
+
+	@Deprecated
+	public void flipHorizontal() {
+		flippedHorizontal = !flippedHorizontal;
+	}
+
+	@Deprecated
+	public void setHorizontalFlip(boolean flippedHorizontal) {
+		this.flippedHorizontal = flippedHorizontal;
 	}
 	
-	public Sprite addSprite(Sprite sprite) {
+	public Sprite addSpriteOld(Sprite sprite) {
 		Sprite[] tmp = spriteSheet;
 		spriteSheet = new Sprite[tmp.length + 1];
 		int i;
@@ -68,35 +100,28 @@ public class SpriteSheet {
 		return (spriteSheet[i] = sprite);
 	}
 
-	public void setSprite(String string) {
+	public void setSpriteOld(String string) {
 		for (int i = 0; i < spriteSheet.length; i++)
 			if (spriteSheet[i].name.equalsIgnoreCase(string)) {
-				/*if (i != sprite)
-					frame = 0;*/
+				/*
+				 * if (i != sprite) frame = 0;
+				 */
 				sprite = i;
 				return;
 			}
 		throw new RuntimeException("No sprite called: " + string);
 	}
 
-	public void setSprite(int i) {
+	public void setSpriteOld(int i) {
 		sprite = i;
 	}
-	
-	public Sprite getSprite(String string) {
+
+	public Sprite getSpriteOld(String string) {
 		for (int i = 0; i < spriteSheet.length; i++)
 			if (spriteSheet[i].name.equalsIgnoreCase(string)) {
 				return spriteSheet[i];
 			}
 		throw new RuntimeException("No sprite called: " + string);
-	}
-
-	public void flipHorizontal() {
-		flippedHorizontal = !flippedHorizontal;
-	}
-
-	public void setHorizontalFlip(boolean flippedHorizontal) {
-		this.flippedHorizontal = flippedHorizontal;
 	}
 
 }
