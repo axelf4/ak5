9ba32949e0f227964cc6489253f4b748f0a6117e
pwnedary <4x31@mail.com>
2013-07-14 20:52:21 +0200

    small changes

--- src/org/gamelib/util/Math2.java
+++ src/org/gamelib/util/Math2.java
@@ -8,7 +8,6 @@ import static java.lang.Math.*;
 /**
  * rename to MathUtils or Math2
  * @author pwnedary
- * 
  */
 public class Math2 {
 
@@ -39,23 +38,25 @@ public class Math2 {
 		return i < min ? min : i > max ? max : i;
 	}
 
-	/** @deprecated in favor of {@link Range}*/
+	/** @deprecated in favor of {@link Range} */
 	@Deprecated
 	public static boolean inRange(int i, int min, int max) {
 		return i <= max && i >= min;
 	}
-	
+
 	public static int log2(int i) {
 		return i == 0 ? 0 : 32 - Integer.numberOfLeadingZeros(i - 1);
 	}
-	
-	/** @return the nearest power of 2*/
+
+	/** @return the nearest power of 2 */
 	public static int pot(int i) {
 		return i == 0 ? 0 : (int) Math.pow(2, 32 - Integer.numberOfLeadingZeros(i - 1));
 	}
-	
-	/** @return the nearest power of 2
-	 * @deprecated replaced by {@link #pot(int)}*/
+
+	/**
+	 * @return the nearest power of 2
+	 * @deprecated replaced by {@link #pot(int)}
+	 */
 	public static int npow2(int i) {
 		return i == 0 ? 0 : (int) Math.pow(2, 32 - Integer.numberOfLeadingZeros(i - 1));
 	}
