7538cdaf63a868fad7dbbc080413138f21ac2df9
pwnedary <4x31@mail.com>
2013-10-21 16:15:29 +0200

    commented and cleaned-up Universe2d

--- src/org/gamelib/Universe2d.java
+++ src/org/gamelib/Universe2d.java
@@ -3,9 +3,7 @@
  */
 package org.gamelib;
 
-import java.util.Arrays;
 import java.util.List;
-import java.util.concurrent.CopyOnWriteArrayList;
 
 import org.gamelib.backend.Graphics;
 
@@ -14,27 +12,24 @@ import org.gamelib.backend.Graphics;
  */
 public class Universe2d implements Handler {
 
-	static final int MAX_ENTITIES = 10000000;
+	static final int INITIAL_SIZE = 10000;
 
 	// public final CopyOnWriteArrayList<Entity> entities = new CopyOnWriteArrayList<Entity>();
-	final Entity[] entities = new Entity[MAX_ENTITIES];
+	/** Array of entities */
+	final Entity[] entities = new Entity[INITIAL_SIZE];
+	/** Number of active entities */
 	int living;
 
-	/**
-	 * 
-	 */
 	public Universe2d() {
 		Registry.instance().register(this);
 	}
 
-	/**
-	 * 
-	 */
 	public Universe2d(Group group) {
 		Registry.instance().register(this, group);
 	}
 
 	/**
+	 * Notifies the entity about updates and draws.
 	 * @return the entity id
 	 */
 	public int spawn(Entity entity) {
@@ -42,49 +37,27 @@ public class Universe2d implements Handler {
 		return living - 1;
 	}
 
+	/** Stops notifying the entity. */
 	public void kill(Entity entity) {
 		for (int i = 0; i < living; i++)
 			if (entities[i].equals(entity)) kill(i);
 	}
 
-	public void kill(int i) {
+	/** Stops notifying the entity with id <code>id</code>. */
+	public void kill(int id) {
 		if (living <= 0) return;
-		living--;
-		entities[i] = entities[living];
+		entities[id] = entities[--living];
 	}
 
+	/** Stops notifying all entities. */
 	public void clear() {
 		this.living = 0;
-	}
-
-	@Deprecated
-	public void add(Entity entity) {
-		// entities.add(entity);
-		entities[living++] = entity;
-	}
-
-	// TODO rename to kill
-	@Deprecated
-	public void remove(Entity entity) {
-		// entities.remove(entity);
-		for (int i = 0; i < getNrEntities(); i++)
-			if (entities[i].equals(entity)) remove(i);
-	}
-
-	@Deprecated
-	public void remove(int i) {
-		// entities.remove(entity);
-		if (living <= 0) return;
-		living--;
-		entities[i] = entities[living];
 	}
 
 	@Override
 	public void handle(Event event) {
 		if (event instanceof Event.Tick) {
 			float delta = ((Event.Tick) event).delta;
-			// for (int i = 0; i < entities.size(); i++) {
-			// Entity entity = entities.get(i);
 			for (int i = 0; i < living; i++) {
 				Entity entity = entities[i];
 				entity.setLastX(entity.getX());
@@ -98,8 +71,6 @@ public class Universe2d implements Handler {
 		} else if (event instanceof Event.Draw) {
 			Graphics g = ((Event.Draw) event).graphics;
 			float delta = ((Event.Draw) event).delta;
-			// for (int i = 0; i < entities.size(); i++) {
-			// Entity entity = entities.get(i);
 			for (int i = 0; i < living; i++) {
 				Entity entity = entities[i];
 				entity.draw(g, delta);
@@ -113,16 +84,21 @@ public class Universe2d implements Handler {
 		list.add(Event.Draw.class);
 	}
 
+	/** @return the array of entities, values CAN be null */
 	public Entity[] getEntities() {
 		// return Arrays.copyOf(entities, living);
 		return entities;
 	}
 
-	/** for use with {@link #getEntities()} */
+	/**
+	 * for use with {@link #getEntities()}
+	 * @return nr of living entities
+	 */
 	public int getNrEntities() {
 		return living;
 	}
 
+	/** @return the next entity's id */
 	public int getNextEntityId() {
 		return living;
 	}
