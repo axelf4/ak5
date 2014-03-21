be0043c71be5b050d2cd501c54b0029b1eba4f36
pwnedary <4x31@mail.com>
2014-03-21 21:47:45 +0100

    err, now refactored

--- axe/src/org/gamelib/Updatable.java
+++ axe/src/org/gamelib/Updatable.java
@@ -3,13 +3,10 @@
  */
 package org.gamelib;
 
-/**
- * Abstraction for "something that can be updated".
- * @author pwnedary
- *
- */
-public interface Updateable {
-
+/** Abstraction for "something that can be updated".
+ * 
+ * @author pwnedary */
+public interface Updatable {
 	/** Updates this object. */
 	public void update(float delta);
 }
